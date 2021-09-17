414a0b4dd7cc1a96f4d69b594d1e61237b6cdcde
PG <anton@samokhvalov.xyz>
2021-09-17 03:33:27 +0300

    better

--- test.cpp
+++ test.cpp
@@ -1,23 +1,20 @@
+#include "dlfcn.h"
+
 #include <cstring>
 #include <cstdlib>
 #include <iostream>
-
-#include "dlfcn.h"
+#include <stdexcept>
 
 int main() {
     auto s = dlsym(RTLD_DEFAULT, "dlerror");
 
     if (!s) {
-        std::cerr << "no sym" << std::endl;
-
-        abort();
+        throw std::runtime_error("no sym");
     }
 
     auto err = ((char* (*)())(s))();
 
     if (strcmp(err, "not found") != 0) {
-        std::cerr << "bad sym" << std::endl;
-
-        abort();
+        throw std::runtime_error("bad sym");
     }
 }
